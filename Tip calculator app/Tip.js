ad64f0f9bba584462619436737e36ae4ff890485
Niks Kozlovs <niks.kozlovs@chi.lv>
2022-10-11 18:44:31 +0300

    Added reset

--- Tip calculator app/Tip.js
+++ Tip calculator app/Tip.js
@@ -1,15 +1,28 @@
 const custom = document.getElementById("custom");
+const tip_per_person = document.getElementById("tip-per-person");
+const total = document.getElementById("total");
+const billAmt = document.getElementById("billAmt");
+const numOfPeople = document.getElementById("numofpeople");
 
 custom.oninput = function(ev) {
     const value = ev.target.value / 100;
     calculateTip(value);
 }
 
+
 function calculateTip(percentage) {
-    const billAmt = document.getElementById("billAmt").value;
-    const numOfPeople = document.getElementById("numofpeople").value;
+    const billAmt = billAmt.value;
+    const numOfPeople = numOfPeople.value;
     const tip = billAmt / numOfPeople * percentage;
     const total = tip * numOfPeople;
-    document.getElementById("tip-per-person").innerHTML = tip.toFixed(2);
-    document.getElementById("total").innerHTML = total.toFixed(2);
+
+    tip_per_person.innerHTML = tip.toFixed(2);
+    total.innerHTML = total.toFixed(2);
+}
+
+function reset() {
+    billAmt.value = "";
+    numOfPeople.value = "";
+    tip_per_person.innerHTML = "0.00";
+    total.innerHTML = "0.00";
 }
